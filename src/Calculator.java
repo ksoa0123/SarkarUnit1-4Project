b3061674d119bcc8e1da1e4e3857baac72b55ae0
kosausrk <kosausrk@gmail.com>
2022-12-06 01:16:25 -0500

    adding stuff

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
     private int num1, num2;
     private String op;
@@ -12,6 +14,91 @@ public class Calculator {
         this.num1 = 5;
         this.num2 = 10;
         this.op = "+";
+    }
+
+    public void calculate(){
+        if (op.equals("/")){
+            System.out.println(divide());
+        }
+
+        if (op.equals("*")){
+            System.out.println(multiply());
+        }
+
+        if (op.equals("-")){
+            System.out.println(subtract());
+        }
+
+        if (op.equals("^")){
+            System.out.println(exponent());
+        }
+
+
+        if (op.equals("+")){
+            System.out.println(addition());
+        }
+
+
+        if (op.equals("%")){
+            System.out.println(mod());
+        }
+    }
+
+    public double divide(){
+        return (double) num1 / num2;
+    }
+
+    public int multiply(){
+        return num1 * num2;
+    }
+
+    public int subtract(){
+        return num1 - num2;
+    }
+
+    public double exponent(){
+        return Math.pow(num1, num2);
+    }
+
+    public int addition(){
+        return num1 + num2;
+    }
+
+    public double mod(){
+        return num1 % num2;
+    }
+
+    public void guessingGame(int maxNum, int maxGuess){ //Max num is the highest number possible and the maxGuess is how many guesses you can make
+        Scanner scan = new Scanner(System.in);
+        // System.out.println(maxNum + " " + maxGuess);
+
+        System.out.println("Enter in your guess: ");
+
+        int userGuess = scan.nextInt();
+
+        System.out.println(userGuess);
+
+        int randomNum = (int) (Math.random() * maxNum); //Generates random number up to range maxNum
+
+        int guessCount = 1; //Counting number of guesses
+
+        while (userGuess != randomNum && guessCount < maxGuess){
+           guessCount += 1;
+
+           System.out.println("Sorry " + userGuess + " was not the correct guess.");
+
+           System.out.println("Enter new guess: ");
+
+           userGuess = scan.nextInt();
+        }
+
+        if (userGuess == randomNum && guessCount < maxGuess){
+            System.out.println("Congrats you found the random number which was: " + randomNum + ". And you did it in " + guessCount + " guesses.");
+        }
+        else{
+            System.out.println("Sorry you lost, you took " + guessCount + " guesses");
+        }
+
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,43 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("\\ Hello welcome to the Mathbot :)  /");
+
         System.out.println("Enter your expression: ");
 
         String expression = scan.nextLine();
 
-        int num1 = Integer.parseInt(expression.substring(0,1));
+        int num1 = Integer.parseInt(expression.substring(0, expression.indexOf(" ")));
 
         int num2 = Integer.parseInt(expression.substring(expression.indexOf(" ") + 3));
 
         String op = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
 
+        System.out.println("Expression Entered: " + num1 + " " +  op + " " +  num2);
+
         Calculator app = new Calculator(num1, num2, op);
+
+        app.calculate();
+
+        System.out.println("Would you like to play the guessing game with Mathbot?");
+
+        String reply = scan.nextLine();
+
+        if (reply.toLowerCase().equals("y") || reply.toLowerCase().equals("yes")){
+            System.out.println("Enter the maximum number for the range you would like: ");
+
+            int userNum = scan.nextInt();
+
+            System.out.println("Enter the maximum guesses you would like: ");
+
+            int userGuess = scan.nextInt();
+
+            app.guessingGame(userNum, userGuess);
+        }
+
+        else{ //If user says no
+            System.out.println("Thank you for using Mathbot!");
+        }
 
 
     }
